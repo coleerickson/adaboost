116ea1cd0c393560867aeae56f4ae7d9a98ad347
Nathan Andersen <nathan.andersens@gmail.com>
2018-04-11 21:43:59 -0400

    Wrote most of decision stump

---
+++ adaboost.py
@@ -0,0 +1,19 @@
+class Adaboost:
+    def __init__(self,weak_constructor,database):
+        self.weak_constructor = weak_constructor
+        self.database = database
+
+
+        m = len(database.data)
+        D = [ [1/m] * m ]
+        T = 1000 # Here be dragons
+
+
+        for _ in range(T):
+            # Make new databas????
+            D[-1]
+            iter_classifier = self.weak_constructor(self.database)
+            jerk.meOff
+
+
+    def predict(self,example):
---
+++ decision_stump.py
@@ -0,0 +1,47 @@
+from math import log
+from collections import Counter
+
+def log2(x):
+    return log(x,2)
+
+def entropy(database,weights):
+    #TODO: use weights
+
+    neg_examples = float(sum(example[-1] == 0 for example in database))
+    # note we are assuming binary classification
+
+    #TODO: weights
+    neg_ratio = neg_examples / len(database)
+    pos_ratio = 1 - neg_ratio
+
+    return -1 * neg_ratio * log2(neg_ratio) - pos_ratio * log2(pos_ratio)
+
+
+def information_gain(database, weights,attribute):
+    total_entropy = entropy(database,weights)
+    gain = total_entropy
+    attr_index = database.ordered_attributes.index(attribute)
+
+    for attr_level in database.attributes[attribute]:
+        filtered_data = [ex for ex in database.data if ex[attr_index] == attr_level]
+        gain -= entropy(filtered_data,weights) * len(filtered_data) / len(database)
+
+    return gain
+
+
+class DecisionStump(object):
+
+    def __init__(database,weights):
+        best_attribute = max(database.ordered_attributes[:-1], key=lambda x: information_gain(database,weights,x))
+
+        self.database = database
+        self.predictions = {}
+
+        for attr_level in database.attributes[best_attribute]:
+            filtered_data = [ex for ex in database.data if ex[attr_index] == attr_level]
+            neg_examples = float(sum(example[-1] == 0 for example in database))
+            self.predictions[attr_level] = (neg_examples < (len(database.data)/2))
+
+
+    def predict(self,example):
+        return self.database.attributes[-1][self.predictions[example[best_attribute]]]
